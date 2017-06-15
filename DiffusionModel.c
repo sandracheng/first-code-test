c959fcd997cdd2c4881bd58929bdf73f4808c097
sandracheng <sandra.cheng1477@gmail.com>
2017-06-15 09:27:07 -0400

    Create DiffusionModel.c

--- DiffusionModel.c
+++ DiffusionModel.c
@@ -18,20 +18,15 @@ Written by Sandra Cheng (10184319) <14wlsc@queensu.ca>
 
 
 /*
-
 imagine the spherical AV; outside we have a layer of water, and  inside we have LAB. Separating the two is an acrylic layer.
-
 we can ignore the effect of gravity on the diffusion; top and bottom can be modelled as the same
-
-this model is 1D. The code is for a particle-wide section of sphere. We drip many particles onto the same spot (continuous dripping), and see the final positions of all of them.
-
-the sphere has symmetry about it, so we can use this code for one particle-wide section to extrapolate for the rest of the sphere 
+this model is 1D. The code is for a particle-wide section of sphere. We drip many particles onto.....
+the same spot (continuous dripping), and see the final positions of all of them.
+the sphere has symmetry about it, so we can use this code for one particle-wide section to extrapolate for 
+the rest of the sphere 
 
  
  
-
-
-
 a particle-wide section of sphere
 
   <--- particle width---->
@@ -68,33 +63,34 @@ int DiffusionModel() {
 
                                         
   /* Need random number r, generated between 0 and 1, to get probability of the walker moving forward or backward.  
-  For now, this model uses prob=0.5 of moving forward and backward, but these numbers should actually change as the particle moves further into the acrylic
+  For now, this model uses prob=0.5 of moving forward and backward, but these numbers should actually change as the..... 
+  particle moves further into the acrylic
   At the beginning, a water molecule is more likely to move forward than backward.
   As x approaches the end of the acrylic, it is equally likely for  molecule to move forward or backward.
   */
   srand(time(NULL));
   int pCount;
                            
-	  for (int pCount = 0; pCount<totParticle; pCount++)  {     // loop through total number of particles,Yan...
-                int dist=0;                                         // counter for the displacement of a single particle. We reset to 0 for each particle.
-                for (int step = 0; step < 1000; step++) {         // loop through steps.  Generate random number b/w 0 and 100 to determine how the particle moves at each step 
-                                                                        
-                        int r = rand()% 100;                     // if r less than  50, walker moves forward 1 space, otherwise it moves back 1 space
-                         //printf("same  %d ", r);
-                 	if (r < 50) {
-                           dist= dist+1;   
-                          //printf("small %d\n",r);
-                        }
-			else {
-                          dist= dist-1;    
-                          //printf("big %d\n",r);
-                        }
-                //printf("%d\n", finalDist[totParticle]);
-               }  // the step loop end curly bracket 
-               finalDist[pCount] = dist;
-               h1->Fill(dist);
-              // printf("counter:%d; final dist:%d\n", pCount, finalDist[pCount]);
-              // sort out the data in a histogram here, since the above comment works. Do it with root commands.
+   for (int pCount = 0; pCount<totParticle; pCount++) { //loop through total number of particles,Yan..
+    int dist=0;                         //counter for the displacement of a single particle. We reset to 0 for each particle.
+    for (int step = 0; step < 1000; step++){  //loop thru steps. Generate random number 0-100 to determine how the particle moves 
+	                                      //at each step                                                                    
+       int r = rand()% 100;              // if r less than  50, walker moves forward 1 space, otherwise it moves back 1 space
+         //printf("same  %d ", r);
+         if (r < 50){
+          dist= dist+1;   
+          //printf("small %d\n",r);
+         }
+	 else {
+          dist= dist-1;    
+          //printf("big %d\n",r);
+         }
+         //printf("%d\n", finalDist[totParticle])
+    }  // the step loop end curly bracket 
+    finalDist[pCount] = dist;
+    h1->Fill(dist);
+    //printf("counter:%d; final dist:%d\n", pCount, finalDist[pCount]);
+    // sort out the data in a histogram here, since the above comment works. Do it with root commands.
                h1->Draw("hist");  
          }  // the total particle loop end curly bracket
   h1->Fit("gaus");                   
